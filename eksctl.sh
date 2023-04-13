976a33950b11cccef6d5246cbe7e96940d5fab87
Rovshan Pashayev <Rovshan.Pashayev@ibm.com>
2023-04-14 01:54:17 +0200

    links

--- eksctl.sh
+++ eksctl.sh
@@ -1,21 +1,22 @@
-aws --version
 
+https://adamtheautomator.com/aws-eks-cli/#Provisioning_your_EKS_Cluster
+https://github.com/kubernetes-sigs/aws-ebs-csi-driver/blob/master/docs/install.md
+
+aws --version
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/bin
 
 eksctl version
-eksctl create cluster --name dev --version 1.24 --region us-east-1 --nodegroup-name standard-workers --node-type t2.micro --nodes 3 --nodes-min 1 --nodes-max 4 --managed
 
 eksctl create cluster --name dev --version 1.25 --region us-east-1 --zones us-east-1a,us-east-1b --node-zones us-east-1a,us-east-1b --nodegroup-name standard-workers --node-type t2.micro --nodes 3 --nodes-min 1 --nodes-max 4 --managed
 
 
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.11/2023-03-17/bin/linux/amd64/kubectl
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.11/2023-03-17/bin/linux/arm64/kubectl
 chmod +x ./kubectl
-
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+aws eks update-kubeconfig --name dev --region us-east-1 
 
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
