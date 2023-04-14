64d77c39aee3f9c8b55897886be7dd5af15a0d08
Rovshan Pashayev <Rovshan.Pashayev@ibm.com>
2023-04-14 23:49:58 +0200

    readme

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # nkdemo
+This repo contains files and instruction to demonstrate following:
+- Setting up AWS EKS Cluster via eksctl tool.
+- Installing MariaDB, Grafana and Elasticsearch services via Helm charts.
+- Getting status of aformentioned services, if they are running, accessible and healthy.
+
+Please read all instruction before implementing it.
+
+Prerequisites:
+
+- AWS Console account
+- Basic knowledege how to operate in AWS console
+- Experinece in launching EC2 instance
+
+Steps:
+
+1. Create AMI Insrance which accessible over internet or console page
+2. Create console user (example name: k8s-admin)
+    a. Attach Admin and follwing policy into the user:
+    https://raw.githubusercontent.com/kubernetes-sigs/aws-ebs-csi-driver/master/docs/example-iam-policy.json
+    b. Set up access and security keys
+    c. Configure aws cli via aws configure command, using keys of new user
+3. Follow and run commands in eksctl.sh file
+    Best practice to run command in batch, in order faster troubleshoot in case of failure
+4. Run helm_install_repos.sh script (bash helm_install_repos.sh)
+    It will download necessary helm repos and install MariaDB, Grafana & Elasticseach microservices
 
 Step 1: Install Kubernetes
 You will need to install Kubernetes on a target environment such as a virtual machine or a cloud instance. You can use a tool such as kubeadm to set up a Kubernetes cluster, or you can use a cloud provider's managed Kubernetes service such as Amazon Elastic Kubernetes Service (EKS), Google Kubernetes Engine (GKE), or Microsoft Azure Kubernetes Service (AKS).
--- eksctl.sh
+++ eksctl.sh
@@ -1,21 +1,25 @@
-###
-aws --version
-aws configure
-
+# setup eksctl command tool
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/bin
 
+# setup kubectl command tool
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.8/2020-04-16/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
 eksctl version
+# following command will take approx. 20 min to run 
+# in case of error either re-run or change AZ
 eksctl create cluster --name dev --region us-east-1 --zones us-east-1a,us-east-1b --node-zones us-east-1a,us-east-1b --nodegroup-name ng-workers --node-type t3.medium --nodes 3 --nodes-min 1 --nodes-max 3 --managed
 
 aws eks update-kubeconfig --name dev --region us-east-1
-#####
-https://github.com/kubernetes-sigs/aws-ebs-csi-driver/blob/master/docs/install.md
+
+# check if you can see worker nodes
+kubectl get nodes
+
+# creating necessary compoenents for EBS CSI Driver
+# source: https://github.com/kubernetes-sigs/aws-ebs-csi-driver/blob/master/docs/install.md
 eksctl utils associate-iam-oidc-provider --region=us-east-1 --cluster=dev --approve
 eksctl create iamserviceaccount \
   --name ebs-csi-controller-sa \
@@ -26,13 +30,16 @@ eksctl create iamserviceaccount \
   --role-only \
   --role-name AmazonEKS_EBS_CSI_DriverRole
 
+# user newly created user's keys
 kubectl create secret generic aws-secret \
     --namespace kube-system \
     --from-literal "key_id=${AWS_ACCESS_KEY_ID}" \
     --from-literal "access_key=${AWS_SECRET_ACCESS_KEY}"
 
+# we will need these tools
 sudo yum install git telnet jq -y
 
+# install helm tool, repos and driver
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
 helm repo update
@@ -41,6 +48,8 @@ helm upgrade --install aws-ebs-csi-driver \
     --namespace kube-system \
     aws-ebs-csi-driver/aws-ebs-csi-driver
 
+# check status of newly created pods, they all should be running
 kubectl get pods -n kube-system -l app.kubernetes.io/name=aws-ebs-csi-driver
 
+# delete eks cluster
 #eksctl delete cluster dev
