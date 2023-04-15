1d252be3dfae8f055d7ee4623083cd2ede5027c3
Rovshan Pashayev <Rovshan.Pashayev@ibm.com>
2023-04-15 11:15:55 +0200

    general edit

--- helm_install_repos.sh
+++ helm_install_repos.sh
@@ -3,7 +3,13 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add elastic https://helm.elastic.co
+sleep 3
 
 helm install grafana grafana/grafana --set service.type=LoadBalancer
+sleep 5
 helm install elasticsearch elastic/elasticsearch --set service.type=LoadBalancer
+sleep 5
 helm install mariadb bitnami/mariadb
+sleep 5
+
+echo "Finished"
