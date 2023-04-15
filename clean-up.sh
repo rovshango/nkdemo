16d835a43d7ba224816054a414d7148daef453ca
Rovshan Pashayev <Rovshan.Pashayev@ibm.com>
2023-04-15 19:24:03 +0200

    delete

--- clean-up.sh
+++ clean-up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Delete stack named eksctl-dev-addon-iamserviceaccount-kube-system-ebs-csi-controller-sa from CloudFourmation page
+aws cloudformation delete-stack --stack-name eksctl-dev-addon-iamserviceaccount-kube-system-ebs-csi-controller-sa
 
 # delete eks cluster
 eksctl delete cluster dev
